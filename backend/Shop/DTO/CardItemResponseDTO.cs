fdeedebd4f39e390e63346fcefa6e1a47a2d5ae4
unknown <lecoqsportif10@mail.ru>
2025-01-20 22:12:38 +0400

    Добавил класс для возвращения, так если  передавать List CartItem, происходит зацикливание и ошибка

---
+++ backend/Shop/DTO/CardItemResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿using Shop.Model;
+
+namespace Shop.DTO
+{
+    public class CardItemResponseDTO
+    {
+        public int CartItemId { get; set; }
+        public int Quantity { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
