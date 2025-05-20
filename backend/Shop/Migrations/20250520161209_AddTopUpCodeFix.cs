4b238505bd7e129262abbac28f3e5be77fd4c823
unknown <lecoqsportif10@mail.ru>
2025-05-20 20:50:59 +0400

    Добавлена обновленная миграция, с добавление новой таблицы  TopUpCode

---
+++ backend/Shop/Migrations/20250520161209_AddTopUpCodeFix.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Shop.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTopUpCodeFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           migrationBuilder.CreateTable(
+           name: "TopUpCodes",
+           columns: table => new
+           {
+               TopUpCodeId = table.Column<int>(nullable: false)
+                   .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+               Code = table.Column<string>(maxLength: 50, nullable: false),
+               Amount = table.Column<int>(nullable: false),
+               IsUsed = table.Column<bool>(nullable: false),
+               CreatedAt = table.Column<DateTime>(nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+               UsedByUserId = table.Column<int>(nullable: true)
+           },
+           constraints: table =>
+           {
+               table.PrimaryKey("PK_TopUpCodes", x => x.TopUpCodeId);
+               table.ForeignKey(
+                   name: "FK_TopUpCodes_Users_UsedByUserId",
+                   column: x => x.UsedByUserId,
+                   principalTable: "Users",
+                   principalColumn: "Id");
+           });
+
+                migrationBuilder.CreateIndex(
+                    name: "IX_TopUpCodes_UsedByUserId",
+                    table: "TopUpCodes",
+                    column: "UsedByUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "TopUpCodes");
+
+        }
+    }
+}
---
+++ backend/Shop/Service/TopUpService.cs
@@ -0,0 +1,52 @@
+﻿using Shop.Interfaces;
+using Shop.Model;
+
+namespace Shop.Service
+{
+    public class TopUpService : ITopUpService
+    {
+        private readonly IRepository<User> _userRepository;
+        private readonly IRepositoryWithUser<BalanceHistory> _balanceHistoryRepository;
+        private readonly ITopUpCodeRepository _codeRepo;
+        private readonly IUserBalanceUpdater _userBalanceUpdater;
+
+        public TopUpService(
+        ITopUpCodeRepository codeRepo,
+        IRepository<User> userRepository,
+        IRepositoryWithUser<BalanceHistory> balanceHistoryRepository,
+        IUserBalanceUpdater userBalanceUpdater)
+        {
+            _codeRepo = codeRepo;
+            _userRepository = userRepository;
+            _balanceHistoryRepository = balanceHistoryRepository;
+            _userBalanceUpdater = userBalanceUpdater;
+        }
+
+        public async Task<bool> ApplyTopUpCodeAsync(string code, int userId, CancellationToken cancellationToken)
+        {
+            var topUpCode = await _codeRepo.GetValidCodeAsync(code, cancellationToken );
+            if (topUpCode == null)
+                return false;
+
+            var user = await _userRepository.GetByIdAsync(userId, cancellationToken);
+            if (user == null)
+                return false;
+
+            user.Balance += (decimal)topUpCode.Amount;
+            await _codeRepo.UseCodeAsync(topUpCode, userId, cancellationToken);
+
+            var history = new BalanceHistory
+            {
+                Amount = (decimal)topUpCode.Amount,
+                Description = $"Пополнение через код {topUpCode.Code}",
+                TransactionDate = DateTime.UtcNow,
+                UserId = userId
+            };
+            await _balanceHistoryRepository.AddAsync(userId, history, cancellationToken);
+
+            await _userBalanceUpdater.UpdateBalanceAsync(user, cancellationToken);
+
+            return true;
+        }
+    }
+}
